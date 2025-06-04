c4e41c4ed629522c560dc783b5d6be53dffe96ba
seunghun-5945 <tmdgns5945@oasis.inje.ac.kr>
2025-06-04 13:44:44 +0900

    feat: 메인 페이지 변경 커밋 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,23 @@
+import Image from "next/image";
+
 const Home = () => {
-  return <>test 입니다</>;
+  return (
+    <div className="w-full h-full">
+      <nav className="flex flex-row items-center justify-between w-full p-6 bg-white">
+        <div className="flex items-center justify-center flex-col">
+          <text className="font-semibold text-3xl">studium</text>
+          <text className="font-semibold text-xl">학습사이트</text>
+        </div>
+        <text className="font-semibold text-xl">signin</text>
+      </nav>
+      <nav className="flex items-center justify-around category w-full p-4 bg-blue-300">
+        <ul>프론트엔드</ul>
+        <ul>백엔드</ul>
+        <ul>서버</ul>
+        <ul>AI</ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Home;
